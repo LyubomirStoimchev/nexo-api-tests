75554134930e956e15e326caab893f66f65e026a
Lyubomir Stoimchev <lyubomirstoimchev@Lyubomirs-MacBook-Air.local>
2024-05-19 21:56:40 +0300

    remove redundant code from config.js

--- config.js
+++ config.js
@@ -1,8 +1,1 @@
-// export default {
-//     BASE_URL: 'https://api.binance.com/api/v3'
-// };
-// const BASE_URL = 'your_base_url';
-
-// export default { BASE_URL };
-
 export const BASE_URL = 'https://api.binance.com/api/v3';
